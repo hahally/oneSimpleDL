c6da4622b38abd51206fb5cb8cd862993fffbb80
hahally <47556859+hahally@users.noreply.github.com>
2022-11-10 20:55:59 +0800

    Create TrainingTrick.py

---
+++ codeline/TrainingTrick.py
@@ -0,0 +1,133 @@
+import torch
+
+class EMA():
+    def __init__(self, model, decay):
+        self.model = model
+        self.decay = decay
+        self.shadow = {}
+        self.backup = {}
+
+    def register(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                self.shadow[name] = param.data.clone()
+
+    def update(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                new_average = (1.0 - self.decay) * param.data + self.decay * self.shadow[name]
+                self.shadow[name] = new_average.clone()
+
+    def apply_shadow(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                self.backup[name] = param.data
+                param.data = self.shadow[name]
+    
+    def restore(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.backup
+                param.data = self.backup[name]
+        self.backup = {}
+
+class FGM():
+    def __init__(self, model):
+        self.model = model
+        self.backup = {}
+    def attack(self, epsilon=0.99, emb_name='embeddings.'):
+        # emb_name这个参数要换成你模型中embedding的参数名
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and emb_name in name:
+                self.backup[name] = param.data.clone()
+                norm = torch.norm(param.grad)
+                if norm != 0 and not torch.isnan(norm):
+                    r_at = epsilon * param.grad / norm
+                    param.data.add_(r_at)
+    def restore(self, emb_name='embeddings.'):
+        # emb_name这个参数要换成你模型中embedding的参数名
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and emb_name in name: 
+                assert name in self.backup
+                param.data = self.backup[name]
+        self.backup = {}
+        
+        
+class FocalLoss(torch.nn.Module):
+    """Multi-class Focal loss implementation"""
+    def __init__(self, gamma=2, weight=None, reduction='mean', ignore_index=-100):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.weight = weight
+        self.ignore_index = ignore_index
+        self.reduction = reduction
+
+    def forward(self, input, target):
+        """
+        input: [N, C]
+        target: [N, ]
+        """
+        log_pt = torch.log_softmax(input, dim=1)
+        pt = torch.exp(log_pt)
+        log_pt = (1 - pt) ** self.gamma * log_pt
+        loss = torch.nn.functional.nll_loss(log_pt, target, self.weight, reduction=self.reduction, ignore_index=self.ignore_index)
+        return loss
+
+
+class AWP:
+    def __init__(self, model, optimizer, *, adv_param='weight',
+                 adv_lr=0.001, adv_eps=0.001):
+        self.model = model
+        self.optimizer = optimizer
+        self.adv_param = adv_param
+        self.adv_lr = adv_lr
+        self.adv_eps = adv_eps
+        self.backup = {}
+        
+    def perturb(self):
+        """
+        Perturb model parameters for AWP gradient
+        Call before loss and loss.backward()
+        """
+        self._save()  # save model parameters
+        self._attack_step()  # perturb weights
+
+    def _attack_step(self):
+        e = 1e-6
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and param.grad is not None and self.adv_param in name:
+                grad = self.optimizer.state[param]['exp_avg']
+                norm_grad = torch.norm(grad)
+                norm_data = torch.norm(param.detach())
+
+                if norm_grad != 0 and not torch.isnan(norm_grad):
+                    # Set lower and upper limit in change
+                    limit_eps = self.adv_eps * param.detach().abs()
+                    param_min = param.data - limit_eps
+                    param_max = param.data + limit_eps
+
+                    # Perturb along gradient
+                    # w += (adv_lr * |w| / |grad|) * grad
+                    param.data.add_(grad, alpha=(self.adv_lr * (norm_data + e) / (norm_grad + e)))
+
+                    # Apply the limit to the change
+                    param.data.clamp_(param_min, param_max)
+                    
+    def _save(self):
+        for name, param in self.model.named_parameters():
+            if param.requires_grad and param.grad is not None and self.adv_param in name:
+                if name not in self.backup:
+                    self.backup[name] = param.clone().detach()
+                else:
+                    self.backup[name].copy_(param.data)
+
+    def restore(self):
+        """
+        Restore model parameter to correct position; AWP do not perturbe weights, it perturb gradients
+        Call after loss.backward(), before optimizer.step()
+        """
+        for name, param in self.model.named_parameters():
+            if name in self.backup:
+                param.data.copy_(self.backup[name])
